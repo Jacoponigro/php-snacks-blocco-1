eaa192c2364a810b261d36d399960b5382a3ab9f
Jacopo <jakonigro@gmail.com>
2020-10-05 17:14:25 +0200

    fine snack 2 ( sintetizzato codice)

--- index2.php
+++ index2.php
@@ -1,34 +1,12 @@
-<!-- dichiaro variabili php e controlli -->
+
 <?php
-// controllo Nome
+$name = $_GET["name"];
+$email = $_GET["email"];
+$age = $_GET["age"];
 
-  $name = $_GET["nome"];
-  if (!strlen($name) > 3) {
-    $name = "Il nome inserito non è valido";
-  }
-  // controllo email
-
-  $email = $_GET["email"];
-  if (!strpos($email, "@") || !strpos($email, ".")) {
-    $email = "L'email inserita non è valida";
-  }
-// controllo età
-
-  $age = $_GET["età"];
-  if (!is_numeric($age)) {
-    $age = "L'età inserita non è valida";
+if((strlen($name) >= 3) && is_numeric($age) && (strpos($email, "@") !== false) && (strpos($email, ".") !== false)) {
+  echo "Accesso riuscito";
+  } else {
+  echo "Accesso negato";
   }
 ?>
-
-<!-- corpo html -->
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>snack 2</title>
-  </head>
-  <body>
-    <h1>Accesso: </h1>
-  </body>
-</html>
